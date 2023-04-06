0275cab12c42884e0bea4f6c477921082f52253d
joaquin <joaquindiaz0000@gmail.com>
2023-04-06 11:28:53 -0300

    Agregue condicional

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ function calcularDescuento(precioTotal, descuento) {
     return Math.abs(precioTotal *  descuento / 100 - precioTotal);
 };
 
+alert("Recuerde que el monto total debe ser mayor a 1000 pesos para que se aplique el descuento")
+
 let precioTotal = 0;
 for (let i = 0; i < totalProductos; i++) {
     let precio = parseInt(prompt("Ingrese el precio de cada producto sin el signo de pesos"));
@@ -11,8 +13,16 @@ for (let i = 0; i < totalProductos; i++) {
     precioTotal = precioTotal + precio;
 }
 
-let descuento = parseInt(prompt("Ingrese el descuento sin el signo de porcentaje"));
-let precioFinal = calcularDescuento(precioTotal, descuento);
+if (precioTotal > "1000") {
+    
+    let descuento = parseInt(prompt("Ingrese el descuento sin el signo de porcentaje"));
+    let precioFinal = calcularDescuento(precioTotal, descuento);
+    alert("El monto final es de " + precioFinal + " pesos.");
 
-alert("El monto final es de " + precioFinal + " pesos.");
+} else {
+    alert("Monto insuficiente")
+}
+
+
+
 
